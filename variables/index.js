2aef31bea3c13c9c58f022a0137f3ef02e6eff60
Hawk1509 <jopaulareeckal@gmail.com>
2024-05-13 08:26:07 +0530

    introduction variables

---
+++ variables/index.js
@@ -0,0 +1,7 @@
+let fullName = "Programmer";
+let age = 25;
+let stud = false;
+
+document.getElementById("p1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent =  `You are ${age} years old`;
+document.getElementById("p3").textContent = `in school: ${stud}`;
