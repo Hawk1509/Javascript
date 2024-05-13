1e46be4f0157bf4f07e5154e74e02a8c788c0996
Hawk1509 <jopaulareeckal@gmail.com>
2024-05-13 06:16:20 +0530

    basics

---
+++ bascs/index.js
@@ -0,0 +1,10 @@
+console.log(`Hello`);
+console.log(`I love Pizza`);
+
+//window.alert("Alert!!");
+//window.alert("I Like pizza");
+
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent =  `i love [izza`;
+
+//
